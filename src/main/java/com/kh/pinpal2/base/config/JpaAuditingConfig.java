bc66ee614de7bb0ff1d2a12994d5af67bba51d75
kangho1870 <ho_gang@naver.com>
2025-08-25 15:36:29 +0900

    Add @EnableJpaRepositories to JpaAuditingConfig to fix UserRepository bean issue

--- src/main/java/com/kh/pinpal2/base/config/JpaAuditingConfig.java
+++ src/main/java/com/kh/pinpal2/base/config/JpaAuditingConfig.java
@@ -2,9 +2,11 @@ package com.kh.pinpal2.base.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "com.kh.pinpal2")
 public class JpaAuditingConfig {
 
 }
