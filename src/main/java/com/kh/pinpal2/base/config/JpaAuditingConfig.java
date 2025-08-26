f8a18831ef45f5e3dd210c17e6281dafd1290367
kangho1870 <ho_gang@naver.com>
2025-08-26 09:12:28 +0900

    Remove @EnableJpaRepositories and autoconfigure exclusions to use default Spring Boot auto-configuration

--- src/main/java/com/kh/pinpal2/base/config/JpaAuditingConfig.java
+++ src/main/java/com/kh/pinpal2/base/config/JpaAuditingConfig.java
@@ -2,11 +2,9 @@ package com.kh.pinpal2.base.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EnableJpaAuditing
-@EnableJpaRepositories(basePackages = "com.kh.pinpal2")
 public class JpaAuditingConfig {
 
 }
